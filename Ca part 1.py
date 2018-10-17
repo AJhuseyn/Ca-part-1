557811e3b095d6ed8a6e4d332098582154b0d695
AJhuseyn <43959986+AJhuseyn@users.noreply.github.com>
2018-10-18 00:03:26 +0100

    Add files via upload

---
+++ Ca part 1.py
@@ -0,0 +1,18 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+class studentRecords(Students):
+    def __init__(self):
+        self.__StudentNumber=''
+        self.__Stu-name=''
+        self.__Stu-surname=''
+        self.__DoB=''
+        self.__location=''
+        self.__CourseID=''
+        self.__Registration=''
+        
+        
+
